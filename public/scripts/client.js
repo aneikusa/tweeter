22d3978b7ac5b95b5cabf6e50212898fcebe1a40
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-10-13 01:05:50 -0400

    Add error logic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,80 +5,155 @@
  */
 
 // Fake data taken from initial-tweets.json
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1665070014923
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1665070014923
-  }
-];
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1665070014923
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1665070014923
+//   }
+// ];
+
+// $(document).ready(function() {
+//   renderTweets(tweetData);
+//   $(".new-text form").submit(function(event) {
+//     event.preventDefault();
+//     console.log($(this).serialize());
+
+//     const tweetText = $(this).find("#tweet-text");
+//     const value = tweetText.val();
+//     const calculateLength = value.length;
+//     const maxLength = 140;
+//     if (value === "") {
+//       alert("Empty tweet, oh no!");
+//       return false;
+//     }
+//     if (calculateLength > maxLength) {
+//       alert("Character count exceeded");
+//       return false;
+//     }
+
+//   });
+
+
+//   function loadTweets() {
+
+//     $.ajax('/tweets', {method: 'GET', dataType: 'JSON'})
+//       .then(function(tweetData) {
+//         console.log("success!:", tweetData);
+//         renderTweets(tweetData);
+//       });
+//   }
+//   loadTweets();
+// });
+// const renderTweets = function(tweets) {
+//   console.log("TWEETS", tweets);
+//   for (const tweet of tweets) {
+//     const element = createTweetElement(tweet);
+//     $('#tweets-container').append(element);
+//   }
+// };
+// const createTweetElement = (tweet) => {
+//   let $tweet =  `<article class="tweet-container">
+//   <header>
+//     <div class="tweet-user">
+//       <img src=${tweet.user.avatars}>
+//       <span class="name-of-user"><strong>${tweet.user.name}</strong></span>
+//     </div>
+//       <span class="user-name"><strong>${tweet.user.handle}</strong></span>
+//   </header>
+//     <div class="tweet-body">${tweet.content.text}</div>
+//   <footer>
+//     <time>${timeago.format(tweet.created_at)}</time>
+//     <div class="icons">
+//       <i class="fas fa-flag"></i>
+//       <i class="fas fa-retweet"></i>
+//       <i class="fas fa-heart"></i>
+//     </div>
+//   </footer>
+// </article>`;
+//   return $tweet;
+// };
 
 $(document).ready(function() {
-  renderTweets(tweetData);
-  $(".new-text form").submit(function(event) {
+  $("#text-form").submit(function(event) {
     event.preventDefault();
-    console.log($(this).serialize());
-
-    const tweetText = $(this).find("#tweet-text");
-    const value = tweetText.val();
-    const calculateLength = value.length;
+    const formData = $(this).serialize();
+    const decodeData = decodeURI(formData).slice(5).length;
     const maxLength = 140;
-    if (value === "") {
-      alert("Empty tweet, oh no!");
-      return false;
-    }
-    if (calculateLength > maxLength) {
-      alert("Character count exceeded");
-      return false;
-    }
+    console.log(decodeData);
 
+    if (!decodeData) {
+      const emptyError = $(".empty-tweet-error").slideDown("slow");
+      return;
+    }
+    if (decodeData > maxLength) {
+      const lengthError = $(".long-tweet-error").slideDown("slow");
+      return;
+    } else {
+      $(".long-tweet-error").slideUp("fast");
+      $(".empty-tweet-error").slideUp("fast");
+    }
+      
+  $.ajax('/tweets', {method: 'POST', data: $(this).serialize()})
+      .then(function() {
+        $('#tweets-container').empty();
+        loadTweets();
+        $("#tweet-text").val('');
+        $('#characters').text(140);
+      });
   });
 
+  const renderTweets = function(tweets) {
+    console.log("TWEETS", tweets);
+    for (const tweet of tweets) {
+      const element = createTweetElement(tweet);
+      $('#tweets-container').prepend(element);
+    }
+  };
 
   function loadTweets() {
-
     $.ajax('/tweets', {method: 'GET', dataType: 'JSON'})
-      .then(function(tweetData) {
+      .then(function (tweetData) {
         console.log("success!:", tweetData);
         renderTweets(tweetData);
       });
   }
   loadTweets();
 });
-const renderTweets = function(tweets) {
-  console.log("TWEETS", tweets);
-  for (const tweet of tweets) {
-    const element = createTweetElement(tweet);
-    $('#tweets-container').append(element);
-  }
+
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 };
+
 const createTweetElement = (tweet) => {
-  let $tweet =  `<article class="tweet-container">
+let $tweet =  `<article class="tweet-container">
   <header>
     <div class="tweet-user">
       <img src=${tweet.user.avatars}>
-      <span class="name-of-user"><strong>${tweet.user.name}</strong></span>
+      <span class="name-of-user"><strong>${escape(tweet.user.name)}</strong></span>
     </div>
-      <span class="user-name"><strong>${tweet.user.handle}</strong></span>
+      <span class="user-name"><strong>${escape(tweet.user.handle)}</strong></span>
   </header>
-    <div class="tweet-body">${tweet.content.text}</div>
+    <div class="tweet-body">${escape(tweet.content.text)}</div>
   <footer>
     <time>${timeago.format(tweet.created_at)}</time>
     <div class="icons">
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,17 @@
-$(document).ready(() => {
-  console.log("DOM loaded");
-});
+$(document).ready(function() {
+  $("#tweet-text")
+    .on("input", function() {
+      //Resize textarea on input
+      this.style.height = "";
+      this.style.height = this.scrollHeight + 4 + "px";
 
-$("#tweet-text").on("input", function () {
-  // Counter value
-  const counter = $(this).closest("form").find(".counter")[0];
-  counter.value = 140 - this.value.length;
+      // Counter value
+      const counter = $(this).closest("form").find(".counter")[0];
+      counter.value = 140 - this.value.length;
 
-  // Counter color change
-  counter.value < 0
-    ? $(counter).css("color", "red")
-    : $(counter).css("color", "#545149");
-
-  // Resize input area
-  this.style.height = "1px";
-  this.style.height = this.scrollHeight + "px";
+      // Counter color change
+      counter.value < 0
+        ? $(counter).css("color", "red")
+        : $(counter).css("color", "#545149");
+    })
 });
