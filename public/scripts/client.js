5087786a4eaf92209b6385263a14ba1ec965401c
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-10-13 00:05:23 -0400

    Serialize form data using jQUERY

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,7 +16,7 @@ const tweetData = [
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    "created_at": 1665070014923
   },
   {
     "user": {
@@ -26,12 +26,26 @@ const tweetData = [
     "content": {
       "text": "Je pense , donc je suis"
     },
-    "created_at": 1461113959088
+    "created_at": 1665070014923
   }
 ]
 
-$( document ).ready(function() {
+$(document).ready(function() {
   renderTweets(tweetData);
+  $( ".new-text form" ).submit(function( event ) {
+    event.preventDefault();
+    console.log($(this).serialize())
+
+  });
+  function loadTweets(){
+
+     $.ajax('/tweets', {method: 'GET', dataType: 'JSON'})
+     .then(function (tweetData) {
+        console.log("success!:", tweetData);
+        renderTweets(tweetData);
+  });
+}
+  loadTweets();
 });
 
 const renderTweets = function(tweets) {
@@ -53,6 +67,7 @@ let $tweet =  `<article class="tweet-container">
   </header>
     <div class="tweet-body">${tweet.content.text}</div>
   <footer>
+    <time>${timeago.format(tweet.created_at)}</time>
     <div class="icons">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
